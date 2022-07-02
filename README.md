3d901f61f3ad1d3067e9d6e87dac4742f947ce0d
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-07-02 20:05:20 +0300

    done

---
+++ README.md
@@ -0,0 +1,2 @@
+1) Pop up for entering 2 numbers and math operation.
+2) Showing in pop up with result of chosen math operation and entered numbers.
---
+++ js/script.js
@@ -0,0 +1,53 @@
+let num1 = prompt("Please enter the first number");
+let num2 = prompt("Please enter the second number");
+let action = prompt("Please choose type of math operation: \n '+' for sum, \n '-' for subtract, \n '*' for multiply, \n ':' or '/' for divide");
+
+if (num1 === null || num1 === '') {
+    alert("You didn't enter the first number. Please try again!");
+}
+
+if (num2 === null || num2 === '') {
+    alert("You didn't enter the second number. Please try again!");
+}
+
+
+function sum(num1,num2){
+    return num1 + num2;
+}
+
+function subtract(num1,num2){
+    return num1 - num2;
+}
+
+function multiply(num1,num2){
+    return num1 * num2;
+}
+
+function divide(num1,num2){
+    return num1 / num2;
+}
+
+if (action === '+') {
+    alert(`Result: ${num1} + ${num2}  = ${sum(num1,num2)}`);
+}
+
+else if (action === '-') {
+    alert(`Result: ${num1} - ${num2}  = ${subtract(num1,num2)}`);
+}
+
+else if (action === '*') {
+    alert(`Result: ${num1} * ${num2}  = ${multiply(num1,num2)}`);
+}
+
+else if (action === ':') {
+    alert(`Result: ${num1} : ${num2}  = ${divide(num1,num2)}`);
+}
+
+else if (action === '/') {
+    alert(`Result: ${num1} : ${num2}  = ${divide(num1,num2)}`);
+}
+
+else {
+    alert('Wrong info were submitted. Please try again!')
+}
+
