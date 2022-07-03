9b30fe6977a78f5475eaa15a88db6c3e7812b430
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-07-03 08:18:26 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let num1 = Number(prompt("Please enter the first number"));
-let num2 = Number(prompt("Please enter the second number"));
-let action = prompt("Please choose type of math operation: \n '+' for sum, \n '-' for subtract, \n '*' for multiply, \n ':' or '/' for divide");
+const num1 = Number(prompt("Please enter the first number"));
+const num2 = Number(prompt("Please enter the second number"));
+const action = prompt("Please choose type of math operation: \n '+' for sum, \n '-' for subtract, \n '*' for multiply, \n ':' or '/' for divide");
 
 if (num1 === null || num1 === 0) {
     alert("You didn't enter the first number. Please try again!");
@@ -50,6 +50,3 @@ else if (action === '/') {
 else {
     alert('Wrong info were submitted. Please try again!')
 }
-
-console.log(typeof num1);
-console.log(typeof num2);
